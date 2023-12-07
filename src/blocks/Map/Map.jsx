c7ca747f9af2b8046918ae2411312e659b9cff65
Antony Kowalski <beheaded_96@mail.ru>
2023-12-08 00:04:05 +0700

    add google maps

--- src/blocks/Map/Map.jsx
+++ src/blocks/Map/Map.jsx
@@ -1,7 +1,9 @@
 import styles from './Map.module.scss';
 
 export const Map = () => {
-  return <section className={styles.map}>
-    
-  </section>;
+  return (
+    <section className={styles.map}>
+      <iframe src="https://snazzymaps.com/embed/552855"></iframe>
+    </section>
+  );
 };
--- src/blocks/SecondFormComponent/SecondFormComponent.jsx
+++ src/blocks/SecondFormComponent/SecondFormComponent.jsx
@@ -8,10 +8,11 @@ export const SecondFormComponent = () => {
       <div className={styles.secondFormComponent__container}>
         <div>
           <h3 className={styles.secondFormComponent__title}>
-            Empowering <br />
+            Empowering <br /> <span>Future Generations</span>
           </h3>
           <p className={styles.secondFormComponent__subtitle}>
-            Our role is to enable our children by <br /> empowering them
+            Our role is to enable our children by <br /> empowering them &nbsp;
+            <span>physically and mentally.</span>
           </p>
         </div>
         <Form title="Join us now" />
